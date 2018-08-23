41998ab421281061b9289e36b09ada77092d4305
soraji <wlthfk0211@gmail.com>
2018-08-23 16:06:57 +0900

    edit

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,4 +2,8 @@ importScripts('/__/firebase/3.8.0/firebase-app.js');
 importScripts('/__/firebase/3.8.0/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
 
-firebase.messaging();
+firebase.initializeApp({
+  'messagingSenderId': '696758097282'
+});
+
+const messaging = firebase.messaging();
